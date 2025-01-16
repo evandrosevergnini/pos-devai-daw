bd81ffa593dc1f3d25b363a62c7c2825365ed0e8
evandro.canal <evandro.canal@tre-es.jus.br>
2025-01-16 15:49:45 -0300

    Criação de botão de logout.

--- Atividade 3/product-client/src/components/NavBar.js
+++ Atividade 3/product-client/src/components/NavBar.js
@@ -1,58 +1,64 @@
-import { getUser } from "../helpers/Utils";
+import { getUser, logout } from "../helpers/Utils";
 import Logo from "../assets/productlogo.png";
-import ReorderIcon from "@mui/icons-material/Reorder"
-import { Link } from "react-router-dom";
+import ReorderIcon from "@mui/icons-material/Reorder";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
-import "../styles/NavBar.css"
+import "../styles/NavBar.css";
 
-export default function NavBar({onLogin}) {
+export default function NavBar({ onLogin }) {
+  const navigate = useNavigate();
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
 
-    const toggleNavbar = () => {
-        setOpenLinks(!openLinks);
-    }
+  const handleLogout = () => {
+    logout();
+    onLogin(false);
+    navigate("/login");
+  };
 
-    const [openLinks, setOpenLinks] = useState(false);
+  const [openLinks, setOpenLinks] = useState(false);
 
-    let userName = getUser();
-    if(userName != null) {
-        return (
-            <div className="navbar">
-                <div className="leftSide" id={openLinks ? "open" : "close"}>
-                    <img src={Logo} />
-                    <div className="hiddenLinks">
-                    <Link to="/home"> Home </Link>
-                    <Link to="/createProduct"> Criar Produto </Link>
-                    </div>
-                </div>
-                <div className="rightSide">
-                    <Link to="/home"> Home </Link>
-                    <Link to="/createProduct"> Criar Produto </Link>
-                    <p>Olá, {userName}</p>
-                    <button onClick={toggleNavbar}>
-                    <ReorderIcon />
-                    </button>
-                </div>
-            </div>
-        );
-    }
-    else {
-        return (
-            <div className="navbar">
-                <div className="leftSide" id={openLinks ? "open" : "close"}>
-                    <img src={Logo} />
-                    <div className="hiddenLinks">
-                    <Link to="/register"> Registrar </Link>
-                    <Link to="/login"> Login </Link>
-                    </div>
-                </div>
-                <div className="rightSide">
-                    <Link to="/register"> Registrar </Link>
-                    <Link to="/login"> Login </Link>
-                    <button onClick={toggleNavbar}>
-                    <ReorderIcon />
-                    </button>
-                </div>
-            </div>
-        );
-    }
+  let userName = getUser();
+  if (userName != null) {
+    return (
+      <div className="navbar">
+        <div className="leftSide" id={openLinks ? "open" : "close"}>
+          <img src={Logo} />
+          <div className="hiddenLinks">
+            <Link to="/home"> Home </Link>
+            <Link to="/createProduct"> Criar Produto </Link>
+          </div>
+        </div>
+        <div className="rightSide">
+          <Link to="/home"> Home </Link>
+          <Link to="/createProduct"> Criar Produto </Link>
+          <p>Olá, {userName}</p>
+          <button onClick={handleLogout}>Logout</button>
+          <button onClick={toggleNavbar}>
+            <ReorderIcon />
+          </button>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="navbar">
+        <div className="leftSide" id={openLinks ? "open" : "close"}>
+          <img src={Logo} />
+          <div className="hiddenLinks">
+            <Link to="/register"> Registrar </Link>
+            <Link to="/login"> Login </Link>
+          </div>
+        </div>
+        <div className="rightSide">
+          <Link to="/register"> Registrar </Link>
+          <Link to="/login"> Login </Link>
+          <button onClick={toggleNavbar}>
+            <ReorderIcon />
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
